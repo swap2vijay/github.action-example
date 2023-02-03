3c2a7b81ccfa0528bc6a333a16d548449dc90cb8
Vijayakumar Thangavel <vijayakumar.thangavel@gmail.com>
2023-02-03 21:19:29 +0000

    code

---
+++ src/main/java/com/concretepage/Main.java
@@ -0,0 +1,20 @@
+package com.concretepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+
+		return "Welcome to Myworld";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
