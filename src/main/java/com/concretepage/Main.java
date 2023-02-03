a040514baead38653b2443815f1f214fb7bb7594
Vijayakumar Thangavel <vijayakumar.thangavel@gmail.com>
2023-02-03 21:24:05 +0000

    Code

--- src/main/java/com/concretepage/Main.java
+++ src/main/java/com/concretepage/Main.java
@@ -18,3 +18,10 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 }
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/swap2vijay/github.action-example.git
+// git push -u origin main
